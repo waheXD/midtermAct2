417f2ad1d5e886e6d52ccfb09f94d2625f0b4c61
gianhailar <164477977+naveaemmanuel@users.noreply.github.com>
2024-04-19 15:02:02 +0800

    Add function to delete

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -16,7 +16,7 @@
           <td>{{ product.description }}</td>
           <td>{{ product.price }}</td>
           <td>
-            <button @click="addToCart(product)">Add to Cart</button>
+            <button @click="deleteProduct(product.id)">Delete</button>
           </td>
         </tr>
       </tbody>
@@ -39,11 +39,11 @@ export default {
         description: "A simple list of ingredients that you probably almost always have on hand in your pantry is used to create a decadent chocolate dessert that can be described as a mix of cake and pudding.",
          price: 350,},
 
-        { id: 2, name: "Cream Cheese Pound Cake", 
+        { id: 3, name: "Cream Cheese Pound Cake", 
         description: "This cream cheese cake recipe takes all of the best parts of a classic pound cake and kicks it up with a creamy, dreamy, and slightly tart ingredient that makes us wonder why we didn't think of this little concoction sooner. ",
          price: 250,},
 
-        { id: 2, name: "Egg Custard Pie", 
+        { id: 4, name: "Egg Custard Pie", 
         description: "Custard pies date back all the way to the Middle Ages, when Romans first discovered the binding properties of eggs. ",
          price: 200,},
 
@@ -59,8 +59,8 @@ export default {
     },
   },
   methods: {
-    addToCart(product) {
-      this.cart.push(product);
+    deleteProduct(id) {
+      this.products = this.products.filter(product => product.id !== id);
     },
   },
 };
@@ -113,4 +113,3 @@ export default {
   background-color: #45a049;
 }
 </style>
-
